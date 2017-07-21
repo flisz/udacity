69b6383ebffc1b927f0d4514f341ec41fa5cead1
fsziebert <franksziebert@gmail.com>
2017-07-21 12:31:37 -0600

    added movie_trailer_website project folder

---
+++ movie_trailer_website/entertainment_center.py
@@ -0,0 +1,18 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+						"A Story of a boy + toys",
+						"https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+						"whee boom zoot suit, blue",
+						"http://www.joblo.com/movie-posters/2009/avatar#image-27654",
+						"https://www.youtube.com/watch?v=d1_JBMrrYw8")
+
+def main():
+	print(avatar.storyline)
+
+if __name__ == '__main__':
+	main()
+

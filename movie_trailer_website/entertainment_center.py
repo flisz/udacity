c639c843a88d42ed73662f92ccb8b4392c94e26a
flisz <franksziebert@gmail.com>
2017-07-22 18:09:56 -0600

    ran webpage generator and fixed poster links to better source, tmdb

--- movie_trailer_website/entertainment_center.py
+++ movie_trailer_website/entertainment_center.py
@@ -9,13 +9,13 @@ toy_story = media.Movie("Toy Story",
 movies.append(toy_story)
 avatar = media.Movie("Avatar",
 						"whee boom zoot suit, blue",
-						"http://www.joblo.com/movie-posters/2009/avatar#image-27654",
+						"https://image.tmdb.org/t/p/w640/tcqb9NHdw9SWs2a88KCDD4V8sVR.jpg",
 						"https://www.youtube.com/watch?v=d1_JBMrrYw8")
 movies.append(avatar)
 
 negotiator = media.Movie("The Negotiator",
 						"Samuel L Jackson and Kevin Spacey Square off in this cop thriller",
-						"http://www.imdb.com/title/tt0120768/mediaviewer/rm2723625216",
+						"https://image.tmdb.org/t/p/w640/uA1zKgJVoO0ljbSbKXyek5YbLwv.jpg",
 						"https://www.youtube.com/watch?v=LJ1pS1W0XXg")
 movies.append(negotiator)
 
